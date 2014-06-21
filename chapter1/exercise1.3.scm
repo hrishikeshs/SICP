2163fa63356a4789b0a72eec4a73845e26d8e089
hrishikeshs <hrish2006@gmail.com>
2014-06-21 21:32:28 +0530

    Exercise 1.3

---
+++ chapter1/exercise1.3.scm
@@ -0,0 +1,9 @@
+;Define a procedure that takes three numbers as arguments and returns the sum of the squares of the two larger numbers
+
+(define (square x) (* x x))
+
+(define (square-of-two-larger-nums x y z)
+  (cond ((and (<= x y) (<= x z)) ( + (square y ) (square z)))
+	((and (<= y x) (<= y z)) (+ (square x) (square z)))
+	((and (<= z x) (<= z y)) (+ (square y) (square x)))))
+
