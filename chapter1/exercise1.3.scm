c7a80332ae7596407a311a6bbc2d3ff2f6b17d03
Hrishikesh S <hrish2006@gmail.com>
2014-06-28 21:59:23 +0530

    indent

--- chapter1/exercise1.3.scm
+++ chapter1/exercise1.3.scm
@@ -3,7 +3,7 @@
 (define (square x) (* x x))
 
 (define (square-of-two-larger-nums x y z)
-  (cond ((and (<= x y) (<= x z)) ( + (square y ) (square z)))
+  (cond ((and (<= x y) (<= x z)) (+ (square y ) (square z)))
 	((and (<= y x) (<= y z)) (+ (square x) (square z)))
 	((and (<= z x) (<= z y)) (+ (square y) (square x)))))
 
