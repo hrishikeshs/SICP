e834680705210e1d0a5bf1957aebb8a439a40b40
hrishikeshs <hrish2006@gmail.com>
2015-05-09 23:50:52 +0530

    wip on the exercise1.12

---
+++ chapter1/exercise1.12.scm
@@ -0,0 +1,50 @@
+;; The following pattern of numbers is called a Pascal's triangle.
+;;               1
+;;             1   1     
+;;           1   2   1      
+;;         1   3   3   1
+;;       1   4   6   4   1
+
+;; The numbers at the edge of the triangle are all 1, and each number
+;; inside the triangle is the sum of the two numbers above it. Write a
+;; procedure that computes elements of the Pascal's triangle by means of a
+;; recursive process.
+
+;; Solution
+
+;; level is the depth to which the triangle has to be generated.
+;; level = 1 means just the first number 1,
+;; level = 2 means: 1, 1, 1
+;; level = 3 means: 1, 1, 1, 1, 2, 1
+;; and so on.
+
+
+(define (helper z)
+  (if (= (length z) 2) (cons (+ (car z) (cadr z)) (list (cadr z)))
+      (list
+       (+ (car z) (cadr z))
+       (helper (cdr z)))))
+
+
+(define (sum-interior x)
+  (cond ((= (length x) 1) (car x))
+        (else
+         (helper x))))
+
+(define (sum-list-pascal x)
+  (if (not (list? x)) (list x)
+      (list (car x) (sum-interior x))))
+
+(define (last x)
+  (if (= (length x) 1)
+      (if (list? (car x)) (car x)
+          x)
+      (last (cdr x))))
+
+(define (pascals-triangle level)
+  (if (= level 1) '(1)
+      (list (pascals-triangle (- level 1))
+            (sum-list-pascal (last (pascals-triangle (- level 1)))))))
+
+
+                              
